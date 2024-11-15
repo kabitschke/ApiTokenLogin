248c2f65644f40963f741baf82854f462b248c7c
Maycon Kabitschke <mayconkabitschke@gmail.com>
2024-11-15 03:06:42 -0300

    login api

---
+++ src/app/login/page.tsx
@@ -0,0 +1,17 @@
+import LoginHandlers from "@/components/login-handlers";
+import LoginServer from "@/components/login-server-action";
+
+
+export default function LoginPage() {
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+
+      <LoginHandlers />
+      {/* <LoginServer/> */}
+    </div>
+  );
+
+}
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,11 +1,38 @@
+import { cookies } from "next/headers";
 import Link from "next/link";
 
+
+type Conta = {
+  autorizado: boolean;
+  usuario: string;
+}
+
 export default async function Menu() {
+
+  let conta: Conta = {
+    autorizado: false,
+    usuario: '',
+  };
+
+  const token = (await cookies()).get('token')?.value;
+
+
+  const response = await fetch('https://api.origamid.online/conta/perfil', {    
+    headers: {
+      Authorization: "Bearer " + token
+    },
+  });
+
+  if (response.ok) {
+    conta = (await response.json()) as Conta;
+
+  }
+
 
   return (
     <ul className="menu">
       <li><Link href={`/`}>Home</Link></li>
-      <li><Link href={`/cursos`}>Cursos</Link></li>
+      <li><Link href={`/login`}>{conta.autorizado ? conta.usuario : 'Login'}</Link></li>
     </ul>
   );
 }
